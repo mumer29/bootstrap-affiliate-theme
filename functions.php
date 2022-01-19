c1e4f4ca9a42acc6b93772ea12a0efdb0d8b57d2
bcorlett201660 <bcorlett201660@gmail.com>
2022-01-19 11:40:15 -0600

    fix shorcodes file path

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php 
 	 add_action( 'wp_enqueue_scripts', 'affiliate_site_enqueue_styles' );
-	 shortcode affiliate_site_enqueue_styles() {
+	 function affiliate_site_enqueue_styles() {
  		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
  		  } 
  		  
- 		  shortcode get_string_between($string, $start, $end){
+ 		  function get_string_between($string, $start, $end){
     $string = ' ' . $string;
     $ini = strpos($string, $start);
     if ($ini == 0) return '';
@@ -12,15 +12,13 @@
     $len = strpos($string, $end, $ini) - $ini;
     return substr($string, $ini, $len);
 }
-
-
 /**
- * Shortcodes
- * Require all PHP files in the /shortcodes/ directory
+ * Functions
+ * Require all PHP files in the /functions/ directory
  */
-foreach (glob(get_template_directory() . "/shortcodes/*.php") as $shortcode) {
-    $shortcode= basename($shortcode);
-    require get_template_directory() . '/shortcodes/' . $shortcode;
+foreach (glob(get_stylesheet_directory() . "/shortcodes/*.php") as $function) {
+    $function= basename($function);
+    require get_stylesheet_directory() . '/shortcodes/' . $function;
 }
 
  ?>
--- shortcodes/hello_world.php
+++ shortcodes/hello_world.php
@@ -1,9 +1,10 @@
 <?php
-add_shortcode('hello_world', 'hello_world_shortcode');
-function hello_world_shortcode( $atts = [], $content = null) {
+add_shortcode('affiliate_hello_world', 'affiliate_hello_world_shortcode');
+function affiliate_hello_world_shortcode( $atts = [], $content = null) {
     // do something to $content
     // always return
-   // return $content;
-   echo 'Hello World';
+    $content = '<p>Hello World</p>';
+    return $content;
 }
+
 ?>
