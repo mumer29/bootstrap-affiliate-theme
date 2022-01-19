ae21171b910790f38c5062a43ed3554633c4d0b6
bcorlett201660 <bcorlett201660@gmail.com>
2022-01-18 22:35:11 -0600

    add afiliate_products shortcode

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php 
 	 add_action( 'wp_enqueue_scripts', 'affiliate_site_enqueue_styles' );
-	 function affiliate_site_enqueue_styles() {
+	 shortcode affiliate_site_enqueue_styles() {
  		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
  		  } 
  		  
- 		  function get_string_between($string, $start, $end){
+ 		  shortcode get_string_between($string, $start, $end){
     $string = ' ' . $string;
     $ini = strpos($string, $start);
     if ($ini == 0) return '';
@@ -13,5 +13,14 @@
     return substr($string, $ini, $len);
 }
 
+
+/**
+ * Shortcodes
+ * Require all PHP files in the /shortcodes/ directory
+ */
+foreach (glob(get_template_directory() . "/shortcodes/*.php") as $shortcode) {
+    $shortcode= basename($shortcode);
+    require get_template_directory() . '/shortcodes/' . $shortcode;
+}
 
  ?>
