5bd11b2e8ad56e7a3deb2b9624071dcae6ad6fc2
bcorlett201660 <bcorlett201660@gmail.com>
2022-01-24 11:09:51 -0600

    move functions under /core and simplify functions.php include

--- functions.php
+++ functions.php
@@ -4,39 +4,15 @@
  		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
  		  } 
  		  
-
-foreach (glob(get_stylesheet_directory() . "/functions/shortcodes/*.php") as $shotcode) {
-    $shotcode= basename($shotcode);
-    require get_stylesheet_directory() . '/functions/shortcodes/' . $shotcode;
-}
-
-foreach (glob(get_stylesheet_directory() . "/functions/admin/notifications/*.php") as $function) {
-    $function= basename($function);
-    require get_stylesheet_directory() . '/functions/admin/notifications/' . $function;
-}
- 
-foreach (glob(get_stylesheet_directory() . "/functions/admin/custom_post_types/*.php") as $function) {
-    $function= basename($function);
-    require get_stylesheet_directory() . '/functions/admin/custom_post_types/' . $function;
-}
-
-foreach (glob(get_stylesheet_directory() . "/functions/admin/taxonomies/*.php") as $function) {
-    $function= basename($function);
-    require get_stylesheet_directory() . '/functions/admin/taxonomies/' . $function;
-}
-
-foreach (glob(get_stylesheet_directory() . "/functions/admin/custom_fields/*.php") as $function) {
-    $function= basename($function);
-   require get_stylesheet_directory() . '/functions/admin/custom_fields/' . $function;
-}
-
-foreach (glob(get_stylesheet_directory() . "/functions/admin/custom_fields/product_info/*.php") as $function) {
-    $function= basename($function);
-   require get_stylesheet_directory() . '/functions/admin/custom_fields/product_info/' . $function;
-}
-
-foreach (glob(get_stylesheet_directory() . "/functions/etc/*.php") as $function) {
-    $function= basename($function);
-   require get_stylesheet_directory() . '/functions/etc/' . $function;
-}
+           function require_all_files($dir) {
+            foreach( glob( "$dir/*" ) as $path ){
+                if ( preg_match( '/\.php$/', $path ) ) {
+                    require_once $path;  // it's a PHP file so just require it
+                } elseif ( is_dir( $path ) ) {
+                    require_all_files( $path );  // it's a subdir, so call the same function for this subdir
+                }
+            }
+        }
+        
+        require_all_files( get_stylesheet_directory() . "/core" );
  ?>
--- functions/etc/siteURL.php
+++
@@ -1,8 +0,0 @@
-<?php
-function siteURL() {
-  $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || 
-    $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-  $domainName = $_SERVER['HTTP_HOST'];
-  return $protocol.$domainName;
-}
-?>
